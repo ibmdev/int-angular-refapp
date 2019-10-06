14845b9abf44976f0336346f46d13c3ede5c4e3f
ibmdev <ibmdev102017@gmail.com>
2019-10-06 16:59:50 +0200

    Intégration composant montant

--- refapp/src/app/app.component.ts
+++ refapp/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MontantComponent } from 'montant';
+import { UploadComponent } from 'upload';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MontantComponent, UploadComponent]
 })
 export class AppComponent {
   title = 'refapp';
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    montant: new FormControl(''),
+    listePiecesJointes: new FormControl([])
+  });
 }
